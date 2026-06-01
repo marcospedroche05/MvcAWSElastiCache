2a549761db8840732c86b29b53948082765ecaae
marcospedroche05 <marcospedroche62@gmail.com>
2026-06-01 13:48:02 +0200

    e

--- MvcAWSElastiCache/Helpers/HelperCache.cs
+++ MvcAWSElastiCache/Helpers/HelperCache.cs
@@ -7,6 +7,7 @@ namespace MvcAWSElastiCache.Helpers
         private static Lazy<ConnectionMultiplexer>
             CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
             {
+                //ESTO ES UNA CADENA DE CONEXION
                 string connectionString = @"cache-coches.p61glr.ng.0001.use1.cache.amazonaws.com:6379,abortConnect=false";
                 //NUESTRA CADENA DE CONEXION
                 return ConnectionMultiplexer.Connect(connectionString);
